4f3d2a215fcaa55d823805c7f745b1190fe7d8a4
Piotr Janik <piotr.janik@primaris.eu>
2015-03-19 01:58:40 +0100

    material

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('primangular', ['ngResource']);
+angular.module('primangular', ['ngResource','ngMaterial']);
--- src/components/gitHubRepo/gitHubDirective.js
+++ src/components/gitHubRepo/gitHubDirective.js
@@ -2,9 +2,21 @@
 angular.module('primangular').directive('gitHub', function () {
   return {
     restrict: 'A',
+    scope: {
+      user: '=gitHub'
+    },
     templateUrl: 'components/gitHubRepo/github.html',
     controller: function ($scope, GitHub) {
-      $scope.repos = GitHub.query();
+
+
+      $scope.getRepos = function () {
+        $scope.repos = GitHub.query({'user': $scope.user});
+      };
+      $scope.$watch('user', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+          $scope.getRepos();
+        }
+      });
     }
   };
 });
